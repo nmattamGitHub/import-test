960d1da2c5ef52c8b906f98effd83dd74be7d4ae
Sébastien Le Callonnec <sebastien@weblogism.com>
2018-03-19 18:14:51 +0000

    PID file is now written by shell script.

--- grails-app/init/docker/example/Application.groovy
+++ grails-app/init/docker/example/Application.groovy
@@ -2,13 +2,9 @@ package docker.example
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import org.springframework.boot.system.ApplicationPidFileWriter
 
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
-        final GrailsApp app = new GrailsApp(Application)
-        // Register PID file writer.
-        app.addListeners(new ApplicationPidFileWriter())
-        app.run(args)
+        GrailsApp.run(Application, args)
     }
 }
